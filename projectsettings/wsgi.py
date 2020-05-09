15bba11fa1160d145bbc8ad3d9144b89c3eb299d
Shikhar Maheshwari <37925362+Shikhar10000@users.noreply.github.com>
2020-05-10 00:00:30 +0530

    Update wsgi.py

--- projectsettings/wsgi.py
+++ projectsettings/wsgi.py
@@ -1,4 +1,4 @@
-"""
+t"""
 WSGI config for test_koalixcrm project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_koalixcrm.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectsettings.settings")
 
 application = get_wsgi_application()
